9f0178b951afdb07b964889fe7dacc6ae20e8006
Ahmed Elgomeay <98486337+ahmedelgomeay@users.noreply.github.com>
2022-04-11 01:15:27 +0200

    Add files via upload
    Final Project Submission after Review

---
+++ SalesInvoiceGenerator/src/com/salesinvoicesgenerator/Main/AppMain.java
@@ -0,0 +1,17 @@
+package com.salesinvoicesgenerator.Main;
+/**
+ *
+ * This is the Main Class. You are free to run the program here or from the Sales Invoices Frame.
+ */
+
+import com.salesinvoicesgenerator.JFrame.SalesInvoicesFrame;
+
+
+public class AppMain {
+    public static void main(String[] args) {
+        SalesInvoicesFrame f = new SalesInvoicesFrame();
+        f.setVisible(true);
+
+    }
+
+}
